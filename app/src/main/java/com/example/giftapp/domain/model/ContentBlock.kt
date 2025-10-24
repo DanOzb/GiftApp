c16096e0d686b9bd047196cfadaa4d216ab4ffb0
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-24 16:49:57 +0200

    feat: Added Content blocks to define list of ordered content to store in room db

--- app/src/main/java/com/example/giftapp/domain/model/ContentBlock.kt
+++ app/src/main/java/com/example/giftapp/domain/model/ContentBlock.kt
@@ -1,4 +1,45 @@
 package com.example.giftapp.domain.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface ContentBlock {
+    val order: Int
 }
+
+@Serializable
+@SerialName("header")
+data class HeaderBlock(
+    override val order: Int = 0,
+    val text: String = ""
+) : ContentBlock
+
+@Serializable
+@SerialName("text_body")
+data class TextBlock(
+    override val order: Int = 0,
+    val text: String = ""
+) : ContentBlock
+
+@Serializable
+@SerialName("image")
+data class ImageBlock(
+    override val order: Int = 0,
+    val url: String = "",
+    val caption: String = ""
+) : ContentBlock
+
+@Serializable
+@SerialName("video")
+data class VideoBlock(
+    override val order: Int = 0,
+    val url: String = ""
+) : ContentBlock
+
+@Serializable
+@SerialName("footer")
+data class FooterBlock(
+    override val order: Int = 0,
+    val text: String = ""
+) : ContentBlock
