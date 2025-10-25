3c646c3851aed190523824799779c3fbef01d1af
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-25 11:33:18 +0200

    feat: implemented dao

--- app/src/main/java/com/example/giftapp/data/local/GiftDao.kt
+++ app/src/main/java/com/example/giftapp/data/local/GiftDao.kt
@@ -1,8 +1,28 @@
 package com.example.giftapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.giftapp.domain.model.GiftEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface GiftDao {
-    
+interface GiftDao{
+    @Query("SELECT * FROM gifts")
+    fun getAllGifts() : Flow<List<GiftEntity>>
+
+    @Query("SELECT * FROM gifts WHERE gifts.favorite=true")
+    fun getFavoriteGifts() : Flow<List<GiftEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(giftEntity: GiftEntity)
+
+    @Update()
+    suspend fun updateGift(giftEntity: GiftEntity)
+
+    @Delete
+    suspend fun delete(giftEntity: GiftEntity)
 }
