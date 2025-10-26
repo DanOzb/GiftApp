6f66f0adfcf170d77d367864b2348a7d55175f0a
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-26 18:22:59 +0100

    fix: updated gallery screen to use OpenGiftScreen

--- app/src/main/java/com/example/giftapp/ui/screens/GalleryScreen.kt
+++ app/src/main/java/com/example/giftapp/ui/screens/GalleryScreen.kt
@@ -8,22 +8,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.giftapp.domain.model.GiftEntity
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.giftapp.viewmodel.GiftViewModel
+
 
 
 @Composable
 fun GalleryScreen(
-    letters: List<GiftEntity>,
-    onClickGift: (GiftEntity) -> Unit){
-}
-
-@Composable
-fun Grid(
-    items: List<GiftEntity>,
-    onItemClick: (GiftEntity) -> Unit,
+    viewmodel: GiftViewModel = hiltViewModel()
 ){
+    val items = viewmodel.gifts.collectAsState().value
+    var openGift by remember { mutableStateOf(false) }
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 100.dp),
         modifier = Modifier.fillMaxSize(),
@@ -34,10 +36,15 @@ fun Grid(
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .clickable { onItemClick(items[index]) }
+                    .clickable { openGift = true }
                     .padding(8.dp)
             ) {
-                //Item content
+                if (openGift){
+                    OpenGiftScreen(
+                        items[index].contentBlocks,
+                        onExit = { openGift = false }
+                    )
+                }
             }
         }
     }
