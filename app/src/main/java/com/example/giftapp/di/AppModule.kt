1d9f88aa1eac9e3029bbd5a66194a95bb7b17d58
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-26 14:06:25 +0100

    feat: Added app module for hilt

--- app/src/main/java/com/example/giftapp/di/AppModule.kt
+++ app/src/main/java/com/example/giftapp/di/AppModule.kt
@@ -1,4 +1,42 @@
 package com.example.giftapp.di
 
-class AppModule {
+import android.content.Context
+import androidx.room.Room
+import com.example.giftapp.data.local.GiftDao
+import com.example.giftapp.data.local.GiftDatabase
+import com.example.giftapp.data.repository.GiftRepositoryImpl
+import com.example.giftapp.domain.repository.GiftRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Binds
+    @Singleton
+    abstract fun bindGiftRepository(
+        giftRepositoryImpl: GiftRepositoryImpl
+    ): GiftRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideDatabase(@ApplicationContext context: Context): GiftDatabase {
+            return Room.databaseBuilder(
+                context,
+                GiftDatabase::class.java,
+                "gift_database"
+            ).build()
+        }
+
+        @Provides
+        fun provideGiftDao(database: GiftDatabase): GiftDao {
+            return database.giftDao()
+        }
+    }
 }
