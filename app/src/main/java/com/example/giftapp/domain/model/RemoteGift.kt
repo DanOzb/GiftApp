77a8d2ce2163e7a81601a2d0ad5ff899cee1dee6
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-26 13:43:36 +0100

    feat: added remote gift for recieving and sending gifts

--- app/src/main/java/com/example/giftapp/domain/model/RemoteGift.kt
+++ app/src/main/java/com/example/giftapp/domain/model/RemoteGift.kt
@@ -1,4 +1,14 @@
 package com.example.giftapp.domain.model
 
-class RemoteGift {
-}
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RemoteGift(
+    @SerialName("giftId")
+    val id: String = "",
+    val title: String = "",
+    val sender: String = "",
+    val timestamp: Long = 0,
+    val contentBlocks: List<@Serializable ContentBlock> = emptyList()
+)
