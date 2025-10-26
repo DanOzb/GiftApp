3fe3f85eaed5868c6add67ece4e103ac20b5e3b5
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-26 13:46:25 +0100

    feat: Added gift repository interface

---
+++ app/src/main/java/com/example/giftapp/domain/repository/GiftRepository.kt
@@ -0,0 +1,17 @@
+package com.example.giftapp.domain.repository
+
+import com.example.giftapp.domain.model.GiftEntity
+import com.example.giftapp.domain.model.RemoteGift
+import kotlinx.coroutines.flow.Flow
+
+interface GiftRepository {
+    val getAllGifts: Flow<List<GiftEntity>>
+    val getFavoriteGifts: Flow<List<GiftEntity>>
+
+    suspend fun addGift(giftEntity: GiftEntity)
+    suspend fun updateGift(giftEntity: GiftEntity)
+    suspend fun deleteGift(giftEntity: GiftEntity)
+
+    suspend fun fetchRemoteGift(giftId: String): RemoteGift?
+    suspend fun toEntity(remoteGift: RemoteGift): GiftEntity
+}
