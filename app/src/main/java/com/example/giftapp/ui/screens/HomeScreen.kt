0c59bb937bb6348acebdae79cd9dbc5fa0be2dac
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-10-27 00:02:45 +0100

    feat: added view model and test gift

--- app/src/main/java/com/example/giftapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/giftapp/ui/screens/HomeScreen.kt
@@ -6,13 +6,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.example.giftapp.ui.navigation.NavScreens
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.giftapp.domain.model.GiftEntity
+import com.example.giftapp.viewmodel.GiftViewModel
+import androidx.compose.runtime.collectAsState
 
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(
+    viewModel: GiftViewModel = hiltViewModel(),
+    ){
+    var showGift by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -20,9 +29,23 @@ fun HomeScreen(navController: NavController){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = {
-            navController.navigate(NavScreens.OpenGift.route)
+            showGift = true
         }) {
+            if(showGift){
+                viewModel.loadAndSaveGift("-1") // Replace with the actual gift ID from firebase
+                viewModel.getGiftById("-1")
+                val gift: GiftEntity? = viewModel.openedGift.collectAsState().value
+                if(gift != null){
+                    OpenGiftScreen(
+                        contentBlocks = gift.contentBlocks,
+                        onExit = { showGift = false }
+                    )
+                } else {
+                    Text("Gift not found")
+                }
+            }
             Text("Open Test Gift")
         }
     }
 }
+
