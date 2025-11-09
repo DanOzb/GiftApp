d55f93fc57d5bf955d80a301e38ae3ba9be52f5a
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-11-09 16:51:05 +0100

    feat: added loading and success/error dialog

--- app/src/main/java/com/example/giftapp/ui/screen/SendGiftScreen.kt
+++ app/src/main/java/com/example/giftapp/ui/screen/SendGiftScreen.kt
@@ -1,6 +1,7 @@
 package com.example.giftapp.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -12,21 +13,28 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -53,20 +61,33 @@ fun SendGiftScreen(
     var showAddMenu by remember { mutableStateOf(false) }
     val items = remember { mutableStateListOf<ContentBlock>() }
 
+    var isLoading by remember { mutableStateOf(false) }
+    var showResultDialog by remember { mutableStateOf(false) }
+    val sendResult by viewModel.sendGiftResult.collectAsState()
+
+    LaunchedEffect(sendResult) {
+        if(sendResult != null) {
+            isLoading = false
+            showResultDialog = true
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("Send Gift") },
                 actions = {
                     Button(onClick = {
-
+                        isLoading = true
                         val remoteGift = RemoteGift(
                             title = "Gift",
                             sender = FirebaseAuth.getInstance().currentUser?.uid ?: "-",
                             contentBlocks = items
                         )
                         viewModel.sendGift(remoteGift)
-                    }) {
+                    },
+                        enabled = !isLoading
+                        ) {
                         Text("Send")
                     }
                 }
@@ -84,9 +105,45 @@ fun SendGiftScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)) {
-            GiftItemsList(
-                items = items,
-            )
+            GiftItemsList(items = items)
+
+            if(isLoading){
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.onSecondary),
+                    contentAlignment = Alignment.Center
+                ){
+                    CircularProgressIndicator()
+                }
+            }
+            if (showResultDialog) {
+                AlertDialog(
+                    onDismissRequest = {
+                        showResultDialog = false
+                        viewModel.resetSendGiftResult()
+                    },
+                    title = {
+                        Text(if (sendResult == true) "Success" else "Error")
+                    },
+                    text = {
+                        Text(if (sendResult == true) "Your gift has been sent successfully!" else "Failed to send the gift. Please try again.")
+                    },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                showResultDialog = false
+                                if (sendResult == true) {
+                                    items.clear() // Clear items on success
+                                }
+                                viewModel.resetSendGiftResult()
+                            }
+                        ) {
+                            Text("OK")
+                        }
+                    }
+                )
+            }
         }
         AddItemMenu(
             expanded = showAddMenu,
