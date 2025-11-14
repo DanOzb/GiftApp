739961f7dc78d2930f3dde347ae3634bc7a51534
Danyal Özbek <enes.ozbek2002@gmail.com>
2025-11-14 13:18:29 +0100

    fix: added vertical pager instead of column

--- app/src/main/java/com/example/giftapp/ui/screen/OpenGiftScreen.kt
+++ app/src/main/java/com/example/giftapp/ui/screen/OpenGiftScreen.kt
@@ -1,15 +1,11 @@
 package com.example.giftapp.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.giftapp.domain.model.ContentBlock
 import com.example.giftapp.ui.screen.components.ContentBlockItem
 import com.example.giftapp.viewmodel.PlayerViewModel
@@ -25,15 +21,17 @@ fun OpenGiftScreen(
         contentBlocks.sortedBy { it.order }
     }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        sortedBlocks.forEach { block ->
-            ContentBlockItem(playerViewModel = playerViewModel, block = block)
-        }
+    val pagerState = rememberPagerState(pageCount = {sortedBlocks.size})
+
+    VerticalPager(
+        state = pagerState,
+        modifier = Modifier.fillMaxSize()
+    ) { page ->
+        val block = sortedBlocks[page]
+        ContentBlockItem(
+            playerViewModel = playerViewModel,
+            block = block,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
